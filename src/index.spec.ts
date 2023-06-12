fac2c5c6b07007f4e1647679c76140d6f800a93f
philippmueller <philippmueller1988@googlemail.com>
2023-06-13 00:04:36 +0200

    task-3: - Tests hinzugefuegt und angepasst.             - Neue Werte zu Array hinzugefügt             - Neue Badges zu Enum hinzugefügt.             - Durch - Infinity werden alle Negatven Zahlen als minium gesetzt dadurch muss die eigentliche Methode nicht angepasst werden.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,28 +4,42 @@ import { User } from './types/user.interface';
 
 describe('getUsersBadge', () => {
 
-  it(`get Gold`, function () {
-    expect(getUsersBadge(getUserMock(100))).toEqual(Icon.BADGE_GOLD);
-    expect(getUsersBadge(getUserMock(50))).toEqual(Icon.BADGE_GOLD);
-    expect(getUsersBadge(getUserMock(1000000))).toEqual(Icon.BADGE_GOLD);
+  it(`get Platinum`, async function () {
+    expect(await getUsersBadge(getUserMock(101))).toEqual(Icon.BADGE_PLATINUM);
+    expect(await getUsersBadge(getUserMock(2000))).toEqual(Icon.BADGE_PLATINUM);
+    expect(await getUsersBadge(getUserMock(1000))).toEqual(Icon.BADGE_PLATINUM);
   });
 
-  it(`get Silver`, function () {
-    expect(getUsersBadge(getUserMock(25))).toEqual(Icon.BADGE_SILVER);
-    expect(getUsersBadge(getUserMock(49))).toEqual(Icon.BADGE_SILVER);
-    expect(getUsersBadge(getUserMock(30))).toEqual(Icon.BADGE_SILVER);
+  it(`get Gold`, async function () {
+    expect(await getUsersBadge(getUserMock(100))).toEqual(Icon.BADGE_GOLD);
+    expect(await getUsersBadge(getUserMock(75))).toEqual(Icon.BADGE_GOLD);
+    expect(await getUsersBadge(getUserMock(50))).toEqual(Icon.BADGE_GOLD);
   });
 
-  it(`get Bronze`, function () {
-    expect(getUsersBadge(getUserMock(5))).toEqual(Icon.BADGE_BRONZE);
-    expect(getUsersBadge(getUserMock(24))).toEqual(Icon.BADGE_BRONZE);
-    expect(getUsersBadge(getUserMock(10))).toEqual(Icon.BADGE_BRONZE);
+  it(`get Silver`, async function () {
+    expect(await getUsersBadge(getUserMock(25))).toEqual(Icon.BADGE_SILVER);
+    expect(await getUsersBadge(getUserMock(49))).toEqual(Icon.BADGE_SILVER);
+    expect(await getUsersBadge(getUserMock(30))).toEqual(Icon.BADGE_SILVER);
   });
 
-  it(`get no Icon`, function () {
-    expect(getUsersBadge(getUserMock(4))).toEqual(null);
-    expect(getUsersBadge(getUserMock(-100))).toEqual(null);
-    expect(getUsersBadge(getUserMock(0))).toEqual(null);
+  it(`get Bronze`, async function () {
+    expect(await getUsersBadge(getUserMock(5))).toEqual(Icon.BADGE_BRONZE);
+    expect(await getUsersBadge(getUserMock(24))).toEqual(Icon.BADGE_BRONZE);
+    expect(await getUsersBadge(getUserMock(10))).toEqual(Icon.BADGE_BRONZE);
+  });
+
+  it(`get Startet`, async function () {
+    expect(await getUsersBadge(getUserMock(4))).toEqual(Icon.BADGE_STARTER);
+    expect(await getUsersBadge(getUserMock(1))).toEqual(Icon.BADGE_STARTER);
+  });
+
+  it(`get Bad Ass`, async function () {
+    expect(await getUsersBadge(getUserMock(-1))).toEqual(Icon.BADGE_BAD_ASS);
+    expect(await getUsersBadge(getUserMock(-1000))).toEqual(Icon.BADGE_BAD_ASS);
+  });
+
+  it(`get null`, async function () {
+    expect(await getUsersBadge(getUserMock(-0))).toEqual(null);
   });
 
 });
--- src/types/icon.enum.ts
+++ src/types/icon.enum.ts
@@ -1,6 +1,10 @@
 export enum Icon {
+  BADGE_BAD_ASS ='badge_bad_ass',
+  BADGE_STARTER ='badge_starter',
   BADGE_BRONZE = 'badge_bronze',
   BADGE_SILVER = 'badge_silver',
   BADGE_GOLD = 'badge_gold',
+  BADGE_PLATINUM = 'badge_platinum',
+  BADGE_GOOD_LIKE = 'badge_good_like',
   DEFAULT = null
 }
